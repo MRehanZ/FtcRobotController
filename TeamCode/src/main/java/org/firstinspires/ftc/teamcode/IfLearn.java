56d64014389f8278e07ba3817e531f080527f364
MRehanZ <mrehan.zarrar@gmail.com>
2025-10-15 17:26:26 -0400

    Update IfLearn.java
    Finished class that will helped me learn about If, Elif, and Else statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfLearn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfLearn.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+
+@Disabled
 @TeleOp
 public class IfLearn extends OpMode {
 
@@ -25,12 +28,28 @@ public class IfLearn extends OpMode {
         telemetry.addData("A Button State", aButton);
         */
         double leftY = gamepad1.left_stick_y;
-        if (leftY < 0) {
-            telemetry.addData("Left Stick", "is Negative");
+        boolean a = gamepad1.a;
+
+        if (leftY < 0.1 && leftY > -0.1) {
+            telemetry.addData("Left Sitck", "In Dead Zone");
+        }
+        if (!a) {
+            telemetry.addData("Speed", "Normal(0.5)");
         }
         else {
-            telemetry.addData("Left Stick", "is Positive");
+            telemetry.addData("Speed", "TURBO!!!");
         }
+        /*
+        if (leftY < 0) {
+            telemetry.addData("Left Stick", "Is Negative");
+        } else if (leftY > 0.5) {
+            telemetry.addData("Left Stick", "Greater than 50%");
+        } else if (leftY > 0) {
+            telemetry.addData("Left Stick", "Is Greater Than 0");
+        } else {
+            telemetry.addData("Left Stick", "Is 0");
+        }
+         */
         telemetry.addData("Left Stick Value", leftY);
 }
 
